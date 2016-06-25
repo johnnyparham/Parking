52b58144b6f6bc230147fbf68b8312984c792688
Johnny Parham <johnny.parham@gmail.com>
2016-06-25 00:29:59 -0400

    Initial Commit

---
+++ Parking/ParkingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ParkingViewController.swift
+//  Parking
+//
+//  Created by Johnny Parham on 6/24/16.
+//  Copyright © 2016 Johnny Parham. All rights reserved.
+//
+
+import UIKit
+
+class ParkingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
