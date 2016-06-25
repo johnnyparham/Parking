443f157b3dd121631c36b95a59515a26526aeba2
Johnny Parham <johnny.parham@gmail.com>
2016-06-25 01:33:24 -0400

    type: Chore
    body: Removed xib file and added storyboard with view controller.

--- Parking/ViewController.swift
+++ Parking/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Parking
 //
-//  Created by Johnny Parham on 6/24/16.
+//  Created by Johnny Parham on 6/25/16.
 //  Copyright © 2016 Johnny Parham. All rights reserved.
 //
 
@@ -12,14 +12,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
